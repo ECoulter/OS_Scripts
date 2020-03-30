07a1fa7d9f6432d566ae59920f995e1c51fda8af
Eric Coulter <jecoulte@iu.edu>
2020-03-30 15:40:51 -0400

    Updates for secgroup and better image name regex'ing

--- create_small_server.sh
+++ create_small_server.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 if [[ -z "$1" ]]; then
-  echo "NO openrc.sh given! Please re-run with ./headnode_create.sh <openrc-name> <server-name>"
+  echo "NO openrc.sh given! Please re-run with ./create_small_server.sh <openrc-name> <server-name>"
   exit
 fi
 
 source ./$1
 
 if [[ -z "$2" ]]; then
-  echo "NO SERVER NAME GIVEN! Please re-run with ./headnode_create.sh <openrc-name> <server-name>"
+  echo "NO SERVER NAME GIVEN! Please re-run with ./create_small_server.sh <openrc-name> <server-name>"
   exit
 fi
 
@@ -24,6 +24,13 @@ else
   server_size="m1.$3"
 fi
 
+if [[ -z "$4" ]]; then
+  echo "NO SECURITY GROUP GIVEN! Please re-run with ./create_small_server.sh <openrc-name> <server-name>"
+  exit
+else
+	secgroup=$4
+fi
+
 if [[ -z "$(openstack network list | grep ${OS_USERNAME}-api-net)" ]]; then
   openstack network create ${OS_USERNAME}-api-net
   openstack subnet create --network ${OS_USERNAME}-api-net --subnet-range 10.0.0.0/24 ${OS_USERNAME}-api-subnet1
@@ -34,10 +41,11 @@ if [[ -z "$(openstack router list | grep ${OS_USERNAME}-api-router)" ]]; then
   openstack router set --external-gateway public ${OS_USERNAME}-api-router
 fi
 
-image_name=$(openstack image list -f value | grep -i JS-API-Featured-Centos7 | grep -v Intel | cut -f 2 -d' ' | head -n 1 )
+#image_name=$(openstack image list -f value | grep -i JS-API-Featured-Centos7 | grep -v Intel | cut -f 2 -d' ' | head -n 1 )
+image_name=$(openstack image list | grep -iE "API-Featured-centos7-[[:alpha:]]{3,4}-[0-9]{2}-[0-9]{4}" | awk '{print $4}')
 
 echo "openstack server create --flavor $server_size  --image $image_name --key-name jecoulte-key --security-group global-ssh --nic net-id=${OS_USERNAME}-api-net $2"
-openstack server create --flavor $server_size  --image $image_name --key-name jecoulte-key --security-group global-ssh --nic net-id=${OS_USERNAME}-api-net $2
+openstack server create --flavor $server_size  --image $image_name --key-name jecoulte-key --security-group $secgroup --nic net-id=${OS_USERNAME}-api-net $2
 
 new_ip=$(openstack floating ip create public | awk '/floating_ip_address/ {print $4}')
 openstack server add floating ip $2 $new_ip
